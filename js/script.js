bd0e922cb6a5ee5a0dbdf0f151cabee7ff413b67
João Victor <joaovictorvieira@outlook.com.br>
2024-09-01 09:55:03 -0400

    ATAs
    lista

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const navbar = document.querySelector('.navbar');
 
     window.addEventListener('scroll', function () {
-        if (window.scrollY > 50) { // Quando o scroll passar de 50px
+        if (window.scrollY > 50) { // Quando o scroll passa de 50px
             navbar.classList.add('navbar-scroll');
         } else {
             navbar.classList.remove('navbar-scroll');
